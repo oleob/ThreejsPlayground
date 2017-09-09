d7962dc726e8239ea427b1a4d67136753a861ea9
oleob <oleob@stud.ntnu.no>
2017-09-09 23:28:20 +0200

    css update

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,9 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ],
   },
   devServer:{
     port: 7777
